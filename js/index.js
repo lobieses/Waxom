fb2bcd2d3a2b8f96701a3a186985e95b4b3394e3
lobieses <76759635+lobieses@users.noreply.github.com>
2021-08-16 06:14:03 +0300

    created latestProjects and videoPresentation blocks

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
-$('.services div').hover((event) => {
-    $(event.currentTarget).toggleClass('isHover');
+$.each($('.filters button'), (index, button) => {               //function for projects filters
+    button.addEventListener('click', (event) => {
+       $('.filters button.active').removeClass('active');
+       event.target.classList.add('active');
+    });
 });
