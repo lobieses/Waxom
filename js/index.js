48a71ca108ddeea19f3144f9958c3d385ecad1c8
lobieses <76759635+lobieses@users.noreply.github.com>
2021-08-22 20:07:01 +0300

    created excellentForMobileDevices/counters/recentPosts blocks

--- js/index.js
+++ js/index.js
@@ -1,6 +1,50 @@
-$.each($('.filters button'), (index, button) => {               //function for projects filters
-    button.addEventListener('click', (event) => {
-       $('.filters button.active').removeClass('active');
-       event.target.classList.add('active');
+const toggleClass = (activeElemsWay, className, addClassElem) => {
+    $(activeElemsWay).removeClass(className);
+    addClassElem.classList.add(className);
+}
+
+const toggle = (elems, className, activeElemsWay) => {
+    for(let elem of elems) {
+        elem.addEventListener('click', (event) => {
+            toggleClass(activeElemsWay, className, event.currentTarget);
+        });
+    }
+}
+
+const changeClassUponArrowsClickAndClicking = (elems, buttons, activeElemsWay, className) => {
+    let indexNumberOfActiveElem = 1;
+
+    for(let elem of elems) {
+        elem.addEventListener('click', (event) => {
+            indexNumberOfActiveElem = event.currentTarget.dataset.num;
+            toggleClass(activeElemsWay, className, elems[indexNumberOfActiveElem]);
+        });
+    }
+
+    buttons[0].addEventListener('click', () => {
+        if (indexNumberOfActiveElem == 0) {
+            indexNumberOfActiveElem = 2;
+        } else {
+            indexNumberOfActiveElem--;
+        }
+        toggleClass(activeElemsWay, className, elems[indexNumberOfActiveElem]);
     });
-});
+
+    buttons[1].addEventListener('click', () => {
+        if (indexNumberOfActiveElem == 2) {
+            indexNumberOfActiveElem = 0;
+        } else {
+            indexNumberOfActiveElem++;
+        }
+
+        toggleClass(activeElemsWay, className, elems[indexNumberOfActiveElem]);
+    });
+}
+
+const posts = document.querySelectorAll('.recentPosts .posts .post')
+const buttons = document.querySelectorAll('.recentPosts .changeButtons .changeButton')
+
+changeClassUponArrowsClickAndClicking(posts, buttons, '.recentPosts .posts .post.active', 'active');
+toggle(document.querySelectorAll('.filters button'), 'active', '.filters button.active'); //function for projects filters
+
+
